f8b52e75d15b96f1e8011f1fb535f7caa7c5c197
juan <jcintasr01@iesalbarregas.es>
2026-04-21 21:27:08 +0200

    Commit provisional

--- trabajofinal_programacion/src/main/java/com/trabajofinal_programacion/controller/SecondaryController.java
+++ trabajofinal_programacion/src/main/java/com/trabajofinal_programacion/controller/SecondaryController.java
@@ -1,12 +1,13 @@
-package com.trabajofinal_programacion;
+package com.trabajofinal_programacion.controller;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import com.trabajofinal_programacion.Main;
 
 public class SecondaryController {
 
     @FXML
     private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+        //Main.setRoot("primary");
     }
 }
