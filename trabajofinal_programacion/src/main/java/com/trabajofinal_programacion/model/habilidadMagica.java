5610720a475310dcf81370c3ef1c8303feeb24e8
juan <jcintasr01@iesalbarregas.es>
2026-04-25 15:15:49 +0200

    Implememntacion de HabilidadMagica.java

--- trabajofinal_programacion/src/main/java/com/trabajofinal_programacion/model/habilidadMagica.java
+++ trabajofinal_programacion/src/main/java/com/trabajofinal_programacion/model/habilidadMagica.java
@@ -1,1 +1,28 @@
 //package com.trabajofinal_programacion.model;
+
+//public class HabilidadMagica implements Habilidad {
+//
+//    @Override
+//    public String getId() { return "magica_area"; }
+//
+//    @Override
+//    public String getNombre() { return "Llama Arcana"; }
+//
+//    @Override
+//    public String getDescripcion() {
+//        return "Daño mágico fijo que ignora defensa.";
+//    }
+//
+//    @Override
+//    public void aplicar(Criatura usuario, Criatura objetivo, StringBuilder log) {
+//        int dano = 25;
+//        int nuevaVida = Math.max(0, objetivo.getStats().getVidaActual() - dano);
+//        objetivo.getStats().setVidaActual(nuevaVida);
+//        log.append(usuario.getNombre())
+//           .append(" lanza ")
+//           .append(getNombre())
+//           .append(" y hace ")
+//           .append(dano)
+//           .append(" de daño mágico.\n");
+//    }
+//}
